229d4d320edb42dfb21f9fcc7214916dbc8007d3
hewenyu <he875027547@126.com>
2018-11-01 13:52:02 +0800

    增加图片选择布局

---
+++ PhotoLayout/src/main/java/com/hwy/photolayout/TakePhotoLayout.java
@@ -0,0 +1,31 @@
+package com.hwy.photolayout;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.ViewGroup;
+
+/**
+ * 作者: hewenyu
+ * 日期: 2018/11/1 13:50
+ * 说明: 选择图片的布局
+ */
+public class TakePhotoLayout extends ViewGroup {
+
+    public TakePhotoLayout(Context context) {
+        super(context);
+    }
+
+    public TakePhotoLayout(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public TakePhotoLayout(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+
+    }
+
+}
